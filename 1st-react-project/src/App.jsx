626f55337e82e1e1dffffdf9fa1c32eb02138882
Chandan01097 <cb131778@gmail.com>
2025-06-05 22:09:18 +0530

    2nd day

--- 1st-react-project/src/App.jsx
+++ 1st-react-project/src/App.jsx
@@ -1,4 +1,5 @@
 import NewApp from './Hero'
+
 function App() {
   return (
     <>
--- 1st-react-project/src/main.jsx
+++ 1st-react-project/src/main.jsx
@@ -1,9 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function My_data() {
+  return (
+    <div>
+      <h1> Hii this is 2nd day of learning react </h1>
+    </div>
+  );
+}
+const Google = () => {
+  return (
+    <a href="https://www.google.com/" target="_blank">
+      Visit Google
+    </a>
+  );
+};
+
+
+createRoot(document.getElementById("root")).render(
+  
+  <Google />
+);
